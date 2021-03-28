78b79e813ab6ff79520ce86040be1a2ff8adda72
chloewang10 <e0420887@u.nus.edu>
2021-03-28 21:10:39 +0800

    Adding the Contact Us and FAQ page

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import Login from './components/Login'
 import Home from './components/Home'
 import SignUp from './components/SignUp'
+import ContactUs from './components/ContactUs'
+import FAQ from './components/FAQ'
 
 
 export default [
     { path: '/', component: Home },
     { path: '/login', component: Login },
     { path: '/signup', component: SignUp },
+    { path: '/contactus', component: ContactUs},
+    { path: '/faq', component: FAQ}
+    
 ]
