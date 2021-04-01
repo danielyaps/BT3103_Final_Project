0fdab85b183855f7ad5ce713687969ecd2905e12
danielyaps <danielyap@Daniels-MacBook-Pro-2.local>
2021-04-01 23:31:28 +0800

    Changes in Sign Up!
    Added fields and change button colour

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,32 +1,59 @@
 <template>
     <div class = "signup"> 
         <form @submit.prevent="pressed">
-            <br><br>
-            <div class="imagePreviewWrapper" :style="{ 'background-image': `url(${previewImage})` }" @click="selectImage">
+            <div class="image-upload">
+                <br>
+                <label for="file-input">
+                    <img :src="this.image">
+                </label>
+                <input id="file-input" type="file" accept="image/*" v-on:change="selectImage">
+                <br>
             </div>
-                <input type="file" accept="image/*" src="previewImage" v-on:change="selectImage">
+            <br>
+            <input type="radio" id="one" value="Student" v-model="type">
+            <label for="one">Student</label> 
+            <input type="radio" id="two" value="Teacher" v-model="type">
+            <label for="two">Teacher</label>
+            <div v-if="this.type === 'Student'">
+                <input type="text" id="user" v-model="username" placeholder="User Name" required>
                 <br>
+                <input type="text" id="fname" v-model="firstname" placeholder="First Name" required> <input type = "text" id="lname" v-model="lastname" placeholder="Last Name" required>
                 <br>
+                <input type="email" id="email" v-model="email" placeholder="Email" required>
+                <br>
+                <input type="password" id="pw" v-model="password" placeholder="Password" required>
+                <br>
+                <input type="text" id="level" v-model="level" placeholder="Level/School" required>
+                <br>
+                <input type="text" id="subjects" v-model="subjects" placeholder="Subjects Interested" required>
+                <br>
+            </div>
+            <div v-else-if="this.type === 'Teacher'">
                 <input type = "text" id="user" v-model="username" placeholder="User Name">
-                <br>
                 <br>
                 <input type = "text" id="fname" v-model="firstname" placeholder="First Name"> <input type = "text" id="lname" v-model="lastname" placeholder="Last Name">
                 <br>
-                <br>
                 <input type = "email" id="em" v-model="email" placeholder="Email">
-                <br>
                 <br>
                 <input type="password" id="pw" v-model="password" placeholder="Password">
                 <br>
+                <input type = "text" id="levels" v-model="levels" placeholder="Level">
                 <br>
-                <input type="radio" id="one" value="Student" v-model="type">
-                <label for="one">Student</label>
-                <input type="radio" id="two" value="Teacher" v-model="type">
-                <label for="two">Teacher</label>
+                <input type = "text" id="subjects" v-model="subjects" placeholder="Subjects Offered">
+                <br>
+                <input type = "text" id="rates" v-model="rates" placeholder="Rates per Hour (SGD)">
                 <br>
                 <br>
-                <button type="submit" v-on:click="register">Create Account</button>
+                <textarea v-model="desc" placeholder="Description" cols="37" rows="5"></textarea>
+                <br>
+            </div>
+            <br>
+            <button type="submit" v-on:click="register">Create Account</button>
         </form>
+        <br>
+        <br>
+        <br>
+        <br>
     </div>
 </template>
 
@@ -37,106 +64,137 @@ import firebaseApp from '../firebase.js'
 export default {
     data() {
         return {
+            uid: "",
+            type: "Student",
             username: "",
             firstname: "",
             lastname: "",
             email: "",
             password: "",
-            type: "",
-            uid: "",
-            previewImage: "https://firebasestorage.googleapis.com/v0/b/bt3103finalproject.appspot.com/o/images%2FScreenshot%202021-03-27%20at%201.38.35%20PM.png?alt=media&token=ae751660-76cc-47c5-a305-543d2f06a1ae",
+            subjects: "",
+            level: "",
+            rates: "",
+            description: "",
+            image: "https://firebasestorage.googleapis.com/v0/b/bt3103finalproject.appspot.com/o/images%2FScreenshot%202021-03-27%20at%201.38.35%20PM.png?alt=media&token=ae751660-76cc-47c5-a305-543d2f06a1ae",
         };
     },
     components: {
     },
     methods: {
         register: function(e) {
-            firebaseApp.auth().createUserWithEmailAndPassword(this.email, this.password)
-                .then((userCredential) => {
+            e.preventDefault();
+            if (this.previewImage != "") {
+                firebaseApp.auth().createUserWithEmailAndPassword(this.email, this.password).then((userCredential) => {
                     var user = userCredential.user;
                     this.uid = user.uid;
-                    firebaseApp.firestore().collection('users').doc(user.uid).set({
+                    if (this.type == "Student") {
+                        firebaseApp.firestore().collection('users').doc(user.uid).set({
+                        type: this.type,
                         username: this.username,
                         firstName: this.firstname,
                         lastName: this.lastname,
                         email: this.email,
-                    });
-                    this.uploadMetadata(this.previewImage);
+                        level: this.level,
+                        subjects: this.subjects
+                        });
+                    } else {
+                        firebaseApp.firestore().collection('users').doc(user.uid).set({
+                        type: this.type,
+                        username: this.username,
+                        firstName: this.firstname,
+                        lastName: this.lastname,
+                        email: this.email,
+                        level: this.level,
+                        subjects: this.subjects,
+                        rates: this.rates,
+                        description: this.description
+                        });
+                    } 
+                    this.uploadMetadata(this.image);
                     this.$router.replace({name:'home'});
                 },
                 err => {
-                    alert(err.message);
+                alert(err.message);
                 });
-                e.preventDefault();
+            } else {
+                alert("Please upload a picture!");
+            }   
         },
+
         selectImage(e){
                 const image = e.target.files[0];
                 const reader = new FileReader();
                 reader.readAsDataURL(image);
                 reader.onload = e =>{
-                    this.previewImage = e.target.result;
+                    this.image = e.target.result;
                 };
         },
+
         uploadMetadata(message) {
             var storageRef = firebaseApp.storage().ref();
             storageRef.child('images/' + this.uid).putString(message, 'data_url');
         }
+
     }
 }
 </script>
 
 <style scoped>
 .signup {
+    position: fixed;
+    overflow-y:scroll;
+    background-color: #E8E8E8;
+    text-align: center;
+    color: #888888;
+    font-family: Roboto;
+    font-size: 14px;
     width: 100vw;
     height: 100vh;
-    background-color: #E8E8E8;
-    text-align: center;
-    color: #888888;
-    font-family: Roboto;
 }
-.imagePreviewWrapper {
+
+.image-upload>input {
+    display: none;
+}
+
+img {
     width: 250px;
     height: 250px;
-    display: block;
-    cursor: pointer;
-    margin: 0 auto 30px;
-    background-size: cover;
-    background-position: center center;
 }
-input[type=text] {
-    background-color: #E8E8E8;
-    border: none;
-    border-bottom: 2px solid #888888;
-    height: 20px;
-    font-family: Roboto;
-    width:145px;
-}
-input#user {
-    width: 300px;
-} 
-input#em{
-    background-color: #E8E8E8;
-    width: 300px;
-    border: none;
-    border-bottom: 2px solid #888888;
-    height: 20px;
-    font-family: Roboto;
-} 
-input#pw{
-    background-color: #E8E8E8;
-    width: 300px;
-    border: none;
-    border-bottom: 2px solid #888888;    
-    height: 20px;
-    color: #888888;
-    font-family: Roboto;
-} 
+
 input[type=radio] {
     text-align: center;
-    margin-left:10px;
+    left: 10px;
+    font-size:14px;
 }
+
+input[type=text], input[type=email],input[type=password] {
+    background-color: #E8E8E8;
+    border: none;
+    border-bottom: 2px solid #888888;
+    padding: 8px;
+    font-family: Roboto;
+    font-size:14px;
+    width:300px;
+    left: 0px;
+}
+
+#fname, #lname {
+    background-color: #E8E8E8;
+    border: none;
+    border-bottom: 2px solid #888888;
+    padding: 10px;
+    font-family: Roboto;
+    font-size:14px;
+    width:135px;
+}
+
+textarea {
+    font-size:14px;
+    font-family: Roboto;
+}
+
 button{
-    background-color: #BEBEBE;
+    background-color: #60C1EA;
     color: white;
     border-radius: 4px;
     width: 130px;
@@ -145,6 +203,4 @@ button{
     font-size: 14px;
     font-family: Roboto;
 }
-
-
 </style>
