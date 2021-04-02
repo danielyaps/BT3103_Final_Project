d2adcb81710acf707b77662fb455ba39069ae00d
Angeleyeo <66460019+Angeleyeo@users.noreply.github.com>
2021-04-02 18:21:08 +0800

    back

--- src/components/HomeStudent.vue
+++ src/components/HomeStudent.vue
@@ -2,13 +2,8 @@
 <div>
     <MenuBarStudents></MenuBarStudents>
     <p style="font-size: 50px; text-align: center; color: #C6C6C6">CLASSY AND FABULOUS TUTORS</p>
-<<<<<<< HEAD
-    <p style="font-size: 30px; color: #C6C6C6; padding-left: 20px">Find Your Tutor Now!</p>
-    <div style="background-color: #E2E2E2; min-height: 500px">
-=======
     <p style="font-size: 30px; color: #C6C6C6; padding-left: 20px; padding-top: 50px">Find Your Tutor Now!</p> <br>
     <div style="background-color: #E2E2E2; min-height: 600px">
->>>>>>> 813d35a9f40e33dd43746f386d3e6e2a5ddb5e86
         <ul id="un">
             <li id="boxed" v-for="tutor of tutors" v-bind:key="tutor.id">
                 <ol style="list-style-type: none">
--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,9 @@ import SignUp from './components/SignUp'
 import ContactUs from './components/ContactUs'
 import FAQ from './components/FAQ'
 import HomeTutor from './components/HomeTutor'
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 import HomeStudent from './components/HomeStudent'
->>>>>>> 813d35a9f40e33dd43746f386d3e6e2a5ddb5e86
 import TutorDetails from './components/TutorDetails'
-=======
-import HomeStudent from './components/HomeStudent'
 
->>>>>>> 3afb6485290c9237ffed2e1fc678727f88848f34
 
 export default [
     { path: '/', name: 'home', component: Home },
@@ -22,13 +15,6 @@ export default [
     { path: '/contactus', component: ContactUs},
     { path: '/faq', component: FAQ},
     { path: '/homeTutor', component: HomeTutor},
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
     { path: '/homeStudent', component: HomeStudent},
->>>>>>> 813d35a9f40e33dd43746f386d3e6e2a5ddb5e86
     { path: '/tutorDetails', component: TutorDetails}
-=======
-    { path: '/homeStudent', component: HomeStudent}
->>>>>>> 3afb6485290c9237ffed2e1fc678727f88848f34
 ]
